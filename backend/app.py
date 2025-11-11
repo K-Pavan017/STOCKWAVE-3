7111ca166bc3dcdc303cf2d78030bf98d8cf50c4
K Pavan <pavanppvss@gmail.com>
2025-11-11 10:58:33 +0530

    app

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
 
 from config import Config
 from waitress import serve
